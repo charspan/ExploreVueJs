4d2f244e74ce23471a9e4098d231ba0e97988679
charspan <1902168804@qq.com>
2017-03-09 16:05:49 +0800

    要新建网关配置页面提交所有更改

--- my-first-vue-project/src/main.js
+++ my-first-vue-project/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'//省略了.vue
+import App from './App' //省略了.vue
 
 // var App = require('./App');
 
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    template: '<App/>',
+    components: { App }
 })
